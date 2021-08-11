4a6e2244b97b8cbdd461f6339ca07b1e01f5929a
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-08-12 02:01:29 +0300

    modified the promise based code

--- src/components/Fform.js
+++ src/components/Fform.js
@@ -2,66 +2,74 @@ import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 require("dotenv").config();
-const PORT = process.env.PORT;
+//const PORT = process.env.PORT;
 //const LIQ_TOKEN = process.env.LIQ_TOKEN;
 export default class Fform extends Component {
   constructor() {
     super();
     this.state = {
+      input: "",
       display_name: "",
       lon: "",
       lat: "",
-      shErr: false,
       cityName: "",
       errExists: false,
       errMessage: "",
       wData: [],
+      shWeather: false,
+      shCity: false,
     };
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      cityName: e.target.children[0].children[0].value,
-    });
-    let url = `https://eu1.locationiq.com/v1/search.php?key=pk.4a782b6f22a6f448625817dfd828280a&q=${this.state.cityName}&format=json`;
+    let url = `https://eu1.locationiq.com/v1/search.php?key=pk.4a782b6f22a6f448625817dfd828280a&q=${this.state.input}&format=json`;
     axios
       .get(url)
       .then((res) => {
         this.setState({
+          shCity: true,
           display_name: res.data[0].display_name,
-          errExists: false,
           lat: res.data[0].lat,
           lon: res.data[0].lon,
-        });
-        //console.log(res.data[0]);
+        }); // end of first .then()
+
         this.props.getCityData(this.state);
-        let urlW = `http://localhost:8080/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.cityName}`;
-        axios
-          .get(urlW)
-          .then((res) => {
-            this.setState({ wData: res.data });
-            console.log(res.data);
-          })
-          .catch((err) => {
-            this.setState({
-              errExists: true,
-              errMessage:
-                "Please check your entry, not found: " + err.response.status,
-            });
-            console.log("errrrrrrrrrrrrror");
-          });
       })
       .catch((err) => {
         if (err.response) {
           this.setState({
+            shCity: false,
+            shWeather: false,
             errExists: true,
             errMessage:
-              "Please check your entry, not found: " + err.response.status,
+              "Please check your entry, not found. ",
           });
-          this.props.getCityData(this.state);
         }
       });
     //let urlW = `http://localhost:${PORT}/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.cityName}`;
+    this.getWeather();
+    this.props.getCityData(this.state);
+  };
+  getWeather = () => {
+    let urlW = `http://localhost:8080/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.input}`;
+    axios
+      .get(urlW)
+      .then((res) => {
+        this.setState({ wData: res.data, shWeather:true });
+      })
+      .catch((err) => {
+        this.setState({
+          shWeather: false,
+          errExists: true,
+          errMessage: "Error retreiving weather data. ",
+        });
+        console.log(err);
+      });
+  };
+  handleInput = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
   };
   render() {
     return (
@@ -76,6 +84,7 @@ export default class Fform extends Component {
               type="text"
               placeholder="Los Angeles"
               className="w-100"
+              onChange={this.handleInput}
             ></input>
           </Form.Group>
           <Button variant="primary" type="submit" className="align-self-center">
